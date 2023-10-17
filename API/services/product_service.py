2cfc7ad35e9ef7f22694590332e69949e1ff28b3
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-10-17 10:57:31 -0300

    Updating register_product and adding list_all_products

--- API/services/product_service.py
+++ API/services/product_service.py
@@ -4,10 +4,14 @@ from API import db
 def register_product(product):
     product_bd = product_model.Product(product_name = product.product_name, 
                                         description = product.description, 
-                                        image = product.image,
                                         quantity = product.quantity, 
                                         regular_price = product.regular_price)
     
     db.session.add(product_bd)
     db.session.commit()
     return product_bd
+
+def list_all_products():
+    products = product_model.Product.query.all()
+    return products
+
