cb4091f620504d25d8fe45fa325324eaacef28cc
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-10-17 10:56:50 -0300

    Removing image

--- API/schemas/product_schema.py
+++ API/schemas/product_schema.py
@@ -3,12 +3,12 @@ from ..models import product_model
 from marshmallow import fields
 
 class ProductSchema(ma.SQLAlchemyAutoSchema):
-    model = product_model.Product
-    load_instance = True
-    fields = ("id", "product_name", "description", "image", "quantity", "regular_price")
+    class Meta:
+        model = product_model.Product
+        load_instance = True
+        fields = ("id", "product_name", "description", "quantity", "regular_price")
 
     product_name = fields.String(required=True)
     description = fields.String(required=True)
-    image = fields.String(required=True)
     quantity = fields.Integer(required=True)
     regular_price = fields.Decimal(required=True)
