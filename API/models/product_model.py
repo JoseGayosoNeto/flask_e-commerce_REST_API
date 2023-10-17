08df6277b96b0b3a0beccb7ae6af8dfeffd3904d
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-10-17 10:55:40 -0300

    Removing image column

--- API/models/product_model.py
+++ API/models/product_model.py
@@ -4,8 +4,7 @@ class Product(db.Model):
     __tablename__ = 'product'
     id = db.Column(db.Integer,primary_key = True, autoincrement=True)
     product_name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.String(100), nullable=False)
-    image = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(100), nullable=False) 
     quantity = db.Column(db.Integer,nullable=False)
     regular_price = db.Column(db.DECIMAL, nullable=False)
 
