b7e33ef013875588ada19f048009cc9225915094
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-11-08 10:39:01 -0300

    Adding cart schema

---
+++ API/schemas/cart_schema.py
@@ -0,0 +1,12 @@
+from API import ma
+from ..models import cart_model
+from marshmallow import fields
+
+class CartSchema(ma.SQLAlchemyAutoSchema):
+    class Meta:
+        model = cart_model.Cart
+        load_instance = True
+        fields = ('product_id', 'quantity')
+
+    product_id = fields.Integer(required=True)
+    quantity = fields.Integer(required=True)    
