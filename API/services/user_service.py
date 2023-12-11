6f152174fcc35a0b91cce15a9709ff55d46ca7b0
José Gayoso Neto <josegayosoneto@outlook.com.br>
2023-12-11 11:56:02 -0300

    Adding update_balance to user service

--- API/services/user_service.py
+++ API/services/user_service.py
@@ -1,8 +1,9 @@
 from API import db
 from ..models import user_model
+from decimal import Decimal
 
 def register_new_user(user):
-    user_bd = user_model.User(name=user.name,email=user.email,password=user.password,address=user.address,is_admin=user.is_admin)
+    user_bd = user_model.User(name=user.name,email=user.email,password=user.password,address=user.address,is_admin=user.is_admin, user_balance=user.user_balance)
     user_bd.encrypt_password()
     db.session.add(user_bd)
     db.session.commit()
@@ -20,4 +21,7 @@ def list_user_by_email(email):
 def delete_user(user):
     db.session.delete(user)
     db.session.commit()
-    
+
+def update_balance(user, value):
+    user.user_balance += Decimal(value)
+    db.session.commit()
