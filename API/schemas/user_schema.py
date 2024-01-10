4b9ec5c2ce2ed3dadf5157e22155bd10a23f2740
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-01-10 10:49:02 -0300

    Adding amount_to_add schema

--- API/schemas/user_schema.py
+++ API/schemas/user_schema.py
@@ -6,13 +6,14 @@ class UserSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = user_model.User
         load_instance = True
-        fields = ("id", "name", "email", "password", "address", "user_balance", "is_admin")
+        fields = ("id", "name", "email", "password", "address", "amount_to_add", "is_admin", "user_balance")
 
     name = fields.String(required=True)
     email = fields.String(required=True)
     password = fields.String(required=True)
     address = fields.String(required=True)
     is_admin = fields.Boolean(required=True)
+    amount_to_add = fields.Decimal(as_string=True)
     user_balance = fields.Decimal(as_string=True)
 
     
