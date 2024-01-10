59087bd2592d5697e6599a283654858b3c3e73fb
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-01-10 11:52:55 -0300

    Adding trasaction_type and transaction_status to the schema

--- API/schemas/transactionItem_schema.py
+++ API/schemas/transactionItem_schema.py
@@ -1,13 +1,13 @@
 from API import ma
 from ..models import transaction_model
-from ..services import product_service
+from ..services import product_service, transaction_service
 from marshmallow import fields
 
 class TransactionItemSchema(ma.SQLAlchemyAutoSchema):
     class Meta:
         model = transaction_model.TransactionItem
         load_instance = True
-        fields = ('transaction_id', 'product_name', 'quantity', 'unit_price', 'total_value')
+        fields = ('transaction_id', 'product_name', 'quantity', 'unit_price', 'total_value', 'transaction_type', 'transaction_status')
     
     transaction_id = fields.Integer(required=True)
     product_id = fields.Integer(required=False)
@@ -15,9 +15,17 @@ class TransactionItemSchema(ma.SQLAlchemyAutoSchema):
     unit_price = fields.Decimal(required=True, as_string=True)
     product_name = fields.Method("get_product_name")
     total_value = fields.Method("get_total_value")
+    transaction_type = fields.Method("get_transaction_type")
+    transaction_status = fields.Method("get_transaction_status")
     
     def get_product_name(self, obj):
         return (product_service.list_product_by_id(obj.product_id)).product_name
     
     def get_total_value(Self, obj):
         return (obj.quantity * obj.unit_price)
+    
+    def get_transaction_type(self,obj):
+        return (transaction_service.list_transaction_by_transaction_id(obj.transaction_id)).transaction_type.name
+    
+    def get_transaction_status(self,obj):
+        return (transaction_service.list_transaction_by_transaction_id(obj.transaction_id)).status.name
