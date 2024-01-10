38291906ecb4cfb151be368019efb41fc1ea56c8
José Gayoso Neto <josegayosoneto@outlook.com.br>
2024-01-10 11:57:57 -0300

    Adding routes for transactions

---
+++ API/views/transaction_views.py
@@ -0,0 +1,38 @@
+from ..entitys import transaction
+from ..services import transaction_service, user_service, transactionItem_service
+from ..schemas import transaction_schema, transactionItem_schema
+from ..models.transaction_model import Transaction
+from API import api
+from flask import make_response,jsonify,request
+from flask_restful import Resource
+from flask_jwt_extended import jwt_required, get_jwt
+from ..decorator import admin_required
+from ..paginate import paginate
+
+class TransactionList(Resource):
+    
+    @admin_required
+    def get(self):
+        ts = transaction_schema.TransactionSchema(many=True)
+        return paginate(Transaction, ts)
+
+class TransactionDetails(Resource):
+    
+    @jwt_required()
+    def get(self,transaction_id):
+        
+        claims = get_jwt()
+        user_id = claims['user_id']
+        
+        is_logged_user_transaction = transaction_service.is_logged_user_transaction(user_id=user_id,transaction_id=transaction_id)
+        if is_logged_user_transaction == False:
+            return make_response(jsonify("Transaction not found."), 404)
+        transaction_items = transactionItem_service.list_transaction_by_id(transaction_id)
+        tis = transactionItem_schema.TransactionItemSchema(many=True)
+        return make_response(tis.jsonify(transaction_items), 200)
+        
+        
+
+api.add_resource(TransactionList, '/transactions')
+api.add_resource(TransactionDetails, '/transactions/transaction/<int:transaction_id>/details')
+            
